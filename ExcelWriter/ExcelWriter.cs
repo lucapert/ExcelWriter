fa7e160435189658eba663a0393ba827481d5118
Luca Perticaroli <luca.perticaroli@dev4side.com>
2020-03-14 00:43:06 +0100

    added other test

--- ExcelWriter/ExcelWriter.cs
+++ ExcelWriter/ExcelWriter.cs
@@ -13,7 +13,7 @@ namespace ExcelWriter
         public void readXLS(string filePath)
         {
             // get the path
-            string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"))+ @"ExcelTemplate\TemplateTest.xlsx";
+            string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\"))+ @"ExcelTemplate\Template.xlsx";
 
             // setting the path
             FileInfo existingFile = new FileInfo(path);
@@ -28,9 +28,9 @@ namespace ExcelWriter
                 // get the first worksheet in the workbook
                 ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
                 // get column count
-                int colCount = worksheet.Dimension.End.Column;
+                //int colCount = worksheet.Dimension.End.Column;
                 // get row count
-                int rowCount = worksheet.Dimension.End.Row;
+                //int rowCount = worksheet.Dimension.End.Row;
                 //for (int row = 1; row <= rowCount; colCount++)
                 //{
                 //    for (int col = 1; col <= colCount; col++) {
@@ -49,15 +49,46 @@ namespace ExcelWriter
                 //Console.WriteLine(worksheet.Cells[1, 1].Value.ToString().Trim());
 
                 // change the first column
-                (worksheet.Cells[1, 1] as ExcelRange).Value = "YESSS!!";
+                (worksheet.Cells["B1"] as ExcelRange).Value = "YESSS!!";
+                string whereToSave = @"C:\Temp\test2.xls";
+                
+                FileInfo fi = new FileInfo(whereToSave);
+                package.SaveAs(fi);
+
+                // for seving from using directive
+                //System.IO.File.WriteAllBytes(@"c:\Temp\myworkbook.xlsx", package);
+
+
+
+                // Other save method
+                //excel.Save(@"C:\Temp\test1.xls");
+                //FileInfo fi = new FileInfo(@"C:\Temp\" + fileName);
+                //package.SaveAs(fi);
+
+                //var filePath = @"C:\Temp\"+fileName;
+                //var mimeType = "application/vnd.ms-excel";
+                //File(new FileStream(filePath, FileMode.Open), mimeType, fileName);
+
+                //// Convert the excel package to a byte array
+                //byte[] bin = package.GetAsByteArray();
+
+                //// create a SeveFileDialog with some properties
+                //SaveFileDialog                
+                //package.Save();
+                //System.IO.File.WriteAllBytes(@"c:\Temp\myworkbook.xlsx", package);
+
+                //package.SaveAs(new FileInfo(@"c:\workbooks\myworkbook.xlsx"));
+
+                // send email
+                //new SMTPEmailManager().SendEmail(new string[] { "luca.perticaroli@dev4side.com" }, new string[0], subject, body, new MemoryStream(package), fileName);
+
                 // save the change
                 package.Save();
 
                 // TODO: Al posto del mio template di esempioinserire quello inviato da Mattia
                 // selezionare il Woprksheet corretto in base al tipo di richiesta (visit request, visit request vip, ...)
                 // Inserire nei campi corrispettivi i valori
-            }
-        
+            }  
         }
     }
 }
